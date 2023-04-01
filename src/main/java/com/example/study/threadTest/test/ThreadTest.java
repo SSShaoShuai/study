b4ae56c45670c496b4676a09c030433a37420f56
Nss-study <472449230@qq.com>
2023-04-01 18:21:46 +0800

    first commit

---
+++ src/main/java/com/example/study/threadTest/test/ThreadTest.java
@@ -0,0 +1,18 @@
+package com.example.study.threadTest.test;
+
+public class ThreadTest {
+    public static void main(String[] args) throws InterruptedException {
+        /* test thread new
+        Thread t1 = new Thread(() -> {
+        });
+        System.out.println(t1.getState());*/
+
+    }
+
+}
+
+
+
+
+
+
